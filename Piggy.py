95f9d02ffe04efc6c1782c4af98c3fb54433fb7a
ArthurConmy <35957051+ArthurConmy@users.noreply.github.com>
2018-03-24 13:19:37 +0000

    any sized grid!

--- Piggy.py
+++ Piggy.py
@@ -6,76 +6,70 @@ white='\033[1;37m'
 black='\033[1;30m'
 reset='\033[0m' ## turn these colours on on repl.it. Not in IDLE tho
 
-def repl_print_game(verticals, horizontals, bs): ##if you're in repl.it, turn this into print_game and delete the function below
+def print_game(horizontals, verticals, rs, cs): ##if you're in repl.it, turn this into print_game and delete the function below
   
-    for index in range(0, bs+1):
-        for index_2 in range(index*bs, index*bs + bs):
+    for index in range(0, rs+1):
+        for index_2 in range(index*cs, (index+1)*cs):
             if horizontals[index_2]==1:
                 print('{} --'.format(white), end='')
             else:
                 print('{} --{}'.format(black, reset), end='') ## colour ?!
         print()
-        if index!=bs:
-            for index_3 in range(index*(bs+1), index*(bs+1) + bs + 1):
+        if index!=rs:
+            for index_3 in range(index*(cs+1), (index+1)*(cs+1)):
                 if verticals[index_3]==1:
                     print('{}|  {}'.format(white, reset), end='')
                 else:
                     print('{}|  {}'.format(black, reset), end='')
         print()
-        
-def print_game(verticals, horizontals, bs):
-  
-    for index in range(0, bs+1):
-        for index_2 in range(index*bs, index*bs + bs):
-            if horizontals[index_2]==1:
-                print(' --', end='')
-            else:
-                print('   ', end='')
-        print()
-        if index!=bs:
-            for index_3 in range(index*(bs+1), index*(bs+1) + bs + 1):
-                if verticals[index_3]==1:
-                    print('|  '.format(white, reset), end='')
-                else:
-                    print('   '.format(black, reset), end='')
-        print()
                 
-def completed_squares(verticals, horizontals, bs): ## Returns the number of completed squares (all players)
+def completed_squares(hs, vs, rs, cs): ## Returns the number of completed squares (all players)
 
-    the_completed_squares=[]
+    the_completed_squares=0 ##=[]
     
-    for upper in range(0, len(horizontals)-bs): ## dependent on board_size !!!
-        if horizontals[upper]==1 and horizontals[upper+bs]==1 and verticals[upper + upper//bs]==1 and verticals[upper + upper//bs + 1]==1:
-            the_completed_squares.append(upper)
+    for upper in range(0, rs*cs): ## dependent on board_size !!!
+        if hs[upper]==1 and hs[upper+cs]==1 and vs[upper + upper//cs]==1 and vs[upper + upper//cs + 1]==1:
+            the_completed_squares+=1 ##.append(upper)
                 
-    return len(the_completed_squares)
+    return the_completed_squares ## used to be len
 
-def is_winnable_square(verticals, horizontals, board_size):
+def is_winnable_square(hs, vs, rs, cs):
   
-  for horizontal in range(0, board_size**2):
-    filled=0
-    spare=-1
-    if horizontals[horizontal]==1:
-      filled+=1
-    else:
-      spare='h'+str(horizontal)
-    if horizontals[horizontal+board_size]==1:
-      filled+=1
-    else:
-      spare='h'+str(horizontal+board_size)
-    if verticals[horizontal + horizontal//board_size]==1:
-      filled+=1
-    else:
-      spare='v'+str(horizontal + horizontal//board_size)
-    if verticals[horizontal + horizontal//board_size + 1]==1:
-      filled+=1 
-    else:
-      spare='v'+str(horizontal + horizontal//board_size + 1)
-    if filled==3:
-      return spare
-  
-  return False
+        ##print(hs)
+        ##print(vs)
 
+        for horizontal in range(0, rs*cs):
+          
+                ##print(horizontal, horizontal + cs, horizontal + horizontal//cs, horizontal + horizontal//cs + 1)
+
+                no_filled = 0
+
+                if hs[horizontal] == 1:
+                        no_filled+=1
+                else:
+                        move = 'h'+str(horizontal)
+
+                if hs[horizontal + cs] == 1:
+                        no_filled+=1
+                else:
+                        move = 'h'+str(horizontal + cs)
+
+                if vs[horizontal + horizontal//cs] == 1:
+                        no_filled+=1
+                else:
+                        move = 'v'+str(horizontal + horizontal//cs)
+
+                if vs[horizontal + horizontal//cs + 1] == 1:
+                        no_filled+=1
+                else:
+                        move = 'v'+str(horizontal + horizontal//cs + 1)
+
+                if no_filled == 3:
+                        return move
+
+        else:
+                return False
+                
 def no_consecutive_takeable_squares(hs, vs, rs, cs): ## take as many squares as possible!
 
     copyh = hs[:]
@@ -105,21 +99,15 @@ def no_consecutive_takeable_squares(hs, vs, rs, cs): ## take as many squares as 
 def random_from_list(lis):
   return lis[randint(0, len(lis)-1)]
       
-board_size=3
+rows=3
+columns=4
 
-hs=[]
-vs=[] ## horizontals and verticals of the board have been initialised
-
-for i in range(0, board_size*(board_size+1)):
-    hs.append(0)
-    vs.append(0)
+hs=[0 for i in range(0, (rows+1)*columns)]
+vs=[0 for i in range(0, (columns+1)*rows)]
 
 no_players=2
 
-our_completed_squares=[]
-
-for do_this in range(0, no_players):
-  our_completed_squares.append(0)
+our_completed_squares=[0 for i in range(0, no_players)]
 
 players_turn=0 ## player 1 is 0, and player 2 is 1. Also this starts at 1 so the loop can begin with the increment
 
@@ -137,9 +125,9 @@ while hs.count(0)+vs.count(0)>0:
     if move_made[0]=='v':
       vs[move_made[1]]=1
 
-    if completed_squares(vs, hs, board_size) > sum(our_completed_squares):
-      print('Player', players_turn+1, 'takes', completed_squares(vs, hs, board_size)-sum(our_completed_squares), 'square(s)! They get another go!')
-      our_completed_squares[players_turn]+=completed_squares(vs, hs, board_size)-sum(our_completed_squares)
+    if completed_squares(hs, vs, rows, columns) > sum(our_completed_squares):
+      print('Player', players_turn+1, 'takes', completed_squares(hs, vs, rows, columns)-sum(our_completed_squares), 'square(s)! They get another go!')
+      our_completed_squares[players_turn]+=completed_squares(hs, vs, rows, columns)-sum(our_completed_squares)
   
     else:
       players_turn+=1
@@ -151,26 +139,27 @@ while hs.count(0)+vs.count(0)>0:
     
     ## if can_square_be_taken==True:
     
-    if is_winnable_square(vs, hs, board_size)!=False:
-      move_made=is_winnable_square(vs, hs, board_size)
+    if is_winnable_square(hs, vs, rows, columns) != False:
+      move_made=is_winnable_square(hs, vs, rows, columns)
     
     ## elif can_square_not_be_given_away==True:
     
-    else: ##Not sure if this is working. Some kind of bug where a move stays locked ... ?
+    else: 
+      
       candidate_moves=[]
       
-      for horizontal in range(0, board_size*(board_size+1)):
+      for horizontal in range(0, columns*(rows+1)):
         if hs[horizontal]!=1:
           copyh=list(hs)
           copyh[horizontal]=1 
-          if is_winnable_square(vs, copyh, board_size)==False:
+          if is_winnable_square(copyh, vs, rows, columns)==False:
             candidate_moves.append('h'+str(horizontal))
 
-      for vertical in range(0, board_size*(board_size+1)):
+      for vertical in range(0, rows*(columns+1)):
         if vs[vertical]!=1:
           copyv=list(vs)
           copyv[vertical]=1 
-          if is_winnable_square(copyv, hs, board_size)==False:
+          if is_winnable_square(hs, copyv, rows, columns)==False:
             candidate_moves.append('v'+str(vertical))
       
       if len(candidate_moves)>0:
@@ -178,12 +167,14 @@ while hs.count(0)+vs.count(0)>0:
         
       else: ## we have to sacrifice. This is done at random but could be done FAR better
         moves=[]
-        for horizontal in range(0, board_size*(board_size+1)):
+        
+        for horizontal in range(0, columns*(rows+1)):
           if hs[horizontal]==0:
             moves.append('h'+str(horizontal))
-        for vertical in range(0, board_size*(board_size+1)):
+        for vertical in range(0, rows*(columns+1)):
           if vs[vertical]==0:
             moves.append('v'+str(vertical))
+        
         move_made=random_from_list(moves)
       
     ## else for move in moves
@@ -195,21 +186,21 @@ while hs.count(0)+vs.count(0)>0:
     if move_made[0]=='v':
       vs[move_made[1]]=1
 
-    if completed_squares(vs, hs, board_size) > sum(our_completed_squares):
-      print('Player', players_turn+1, 'takes', completed_squares(vs, hs, board_size)-sum(our_completed_squares), 'square(s)! They get another go!')
-      our_completed_squares[players_turn]+=completed_squares(vs, hs, board_size)-sum(our_completed_squares)
+    if completed_squares(hs, vs, rows, columns) > sum(our_completed_squares):
+      print('Player', players_turn+1, 'takes', completed_squares(hs, vs, rows, columns)-sum(our_completed_squares), 'square(s)! They get another go!')
+      our_completed_squares[players_turn]+=completed_squares(hs, vs, rows, columns)-sum(our_completed_squares)
   
     else:
       players_turn+=1
       players_turn=players_turn%2
       
-      
-  print_game(vs, hs, board_size)
+  print_game(hs, vs, rows, columns)
 
 if our_completed_squares[0]>our_completed_squares[1]:
   print('You win!')
   
 else:
+  
   if our_completed_squares[0]<our_completed_squares[1]:
     print('Computer wins')
     
